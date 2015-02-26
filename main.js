0c9d12596ce98ec9835830a54174f1e29da864cb
Mateo Gianolio <gianoliomateo@gmail.com>
2015-02-26 15:35:08 +0100

    add dirname to avoid file not found errors | remove unnecessary letters array

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('db/words.db');
-
-var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var db = new sqlite3.Database(__dirname + '/db/words.db');
 
 exports.get = function(string, callback) {
   var time = process.hrtime();
